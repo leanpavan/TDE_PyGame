6d0d08d49e0a804079cafa0663f0b4cbe4fdac11
Leandro Pavan <66214396+leanpavan@users.noreply.github.com>
2024-03-25 08:29:37 -0300

    main

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import pygame
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, "data")
+
+circle = pygame.image.load(os.path.join("data", "circle.png"))
+circlerect = circle.get_rect()
+
+#pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
+delta_time = 0
+
+player_pos = pygame.Vector2(screen.get_width())
+
+while running:
+    #poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    screen.blit(circle, circlerect)
+    pygame.display.flip()
+
+    delta_time = clock.tick(60) / 1000
+
+pygame.quit()
