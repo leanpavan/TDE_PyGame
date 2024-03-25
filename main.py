fc4f8362545f56c16361c670129fd88e3a38af92
Leandro Pavan <lejpavan17@gmail.com>
2024-03-25 09:17:27 -0300

    added circle in center of the screen

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import pygame
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "data")
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_CENTER = 400, 300
+
 circle = pygame.image.load(os.path.join("data", "circle.png"))
+circle = pygame.transform.scale(circle, (128, 128))
 circlerect = circle.get_rect()
+circlerect.center = (SCREEN_CENTER)
 
 #pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 delta_time = 0
@@ -23,7 +29,7 @@ while running:
             running = False
 
     #fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill("gray")
     screen.blit(circle, circlerect)
     pygame.display.flip()
 
